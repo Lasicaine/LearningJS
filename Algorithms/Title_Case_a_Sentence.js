c55fec9a371bef02451621da0bbc90eb9f6b38c6
Suomaa <a.antomonov@mail.ru>
2016-10-19 18:42:35 +0300

    Change tabulations

--- Algorithms/Title_Case_a_Sentence.js
+++ Algorithms/Title_Case_a_Sentence.js
@@ -1,10 +1,10 @@
 function titleCase(str) {
-  var arrayOfWords = str.split(' ');
-  for (var i = 0, arrLength = arrayOfWords.length; i < arrLength; i++) {
-    arrayOfWords[i] = arrayOfWords[i].charAt(0).toUpperCase() + arrayOfWords[i].slice(1).toLowerCase();
-  }
-  var titleCaseString = arrayOfWords.join(' ');
-  return titleCaseString;
+    var arrayOfWords = str.split(' ');
+    for (var i = 0, arrLength = arrayOfWords.length; i < arrLength; i++) {
+        arrayOfWords[i] = arrayOfWords[i].charAt(0).toUpperCase() + arrayOfWords[i].slice(1).toLowerCase();
+    }
+    var titleCaseString = arrayOfWords.join(' ');
+    return titleCaseString;
 }
 
 titleCase("I'm a little tea pot");
