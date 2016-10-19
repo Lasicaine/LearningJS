ca3a0cea303274efed74a2f0ab6809b3818000a0
Suomaa <a.antomonov@mail.ru>
2016-10-19 18:41:32 +0300

    + micro change Title Case a Sentence algprithm

--- Algorithms/Title_Case_a_Sentence.js
+++ Algorithms/Title_Case_a_Sentence.js
@@ -1,7 +1,6 @@
 function titleCase(str) {
   var arrayOfWords = str.split(' ');
-  var arrLength = arrayOfWords.length;
-  for (var i = 0; i < arrLength; i++) {
+  for (var i = 0, arrLength = arrayOfWords.length; i < arrLength; i++) {
     arrayOfWords[i] = arrayOfWords[i].charAt(0).toUpperCase() + arrayOfWords[i].slice(1).toLowerCase();
   }
   var titleCaseString = arrayOfWords.join(' ');
